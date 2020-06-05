40901132f887e8760d1deb00fcb319070715b0b8
Kym Andrew <flamingpearl27@gmail.com>
2020-06-05 16:50:08 +0100

    restructuring of -s page structure up to the point of floating main content and widget.  Feels like now's the time to branch

--- footer.php
+++ footer.php
@@ -10,23 +10,28 @@
  */
 
 ?>
+	<div class="footer-section full">
+		<div class="wrap">
+			<footer id="colophon" class="site-footer inner">
+				<div class="site-info">
+					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'daminhahorta' ) ); ?>">
+						<?php
+						/* translators: %s: CMS name, i.e. WordPress. */
+						printf( esc_html__( 'Proudly powered by %s', 'daminhahorta' ), 'WordPress' );
+						?>
+					</a>
+					<span class="sep"> | </span>
+						<?php
+						/* translators: 1: Theme name, 2: Theme author. */
+						printf( esc_html__( 'Theme: %1$s by %2$s.', 'daminhahorta' ), 'daminhahorta', '<a href="http://underscores.me/">Flaming Pearl</a>' );
+						?>
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
+		</div>
+	</div>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'daminhahorta' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'daminhahorta' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'daminhahorta' ), 'daminhahorta', '<a href="http://underscores.me/">Flaming Pearl</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+
+
 
 <?php wp_footer(); ?>
 
